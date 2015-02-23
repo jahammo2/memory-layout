846bb5c846214ebfebfafd09c68be54aeb8797b9
jahammo2 <jahammo2@gmail.com>
2015-02-23 07:47:14 -0500

    beautified javascript

--- js/main.js
+++ js/main.js
@@ -1,26 +1,26 @@
-$(function () {
+$(function() {
 
-	var counter;
-	var msInt;
-	var secInt;
-	var ms;
-	var seconds;
+    var counter;
+    var msInt;
+    var secInt;
+    var ms;
+    var seconds;
 
-	afterDiv().firstPage();
+    afterDiv().firstPage();
 
-	$('.hard').on('click', function() {
-		afterDiv().secondPage();
-		gamePlay(12, 12);
-		gameSetup().hardGame();
-		for (var i = 2; i >= 0; i--) {
-			$('.fa-heart:last').remove();
-		};
-	})
+    $('.hard').on('click', function() {
+        afterDiv().secondPage();
+        gamePlay(12, 12);
+        gameSetup().hardGame();
+        for (var i = 2; i >= 0; i--) {
+            $('.fa-heart:last').remove();
+        };
+    })
 
-	$('.easy').on('click', function() {
-		afterDiv().secondPage();
-		$('.hard-memory-row').hide();
-		gamePlay(9, 15);
-		gameSetup().easyGame();
-	})
+    $('.easy').on('click', function() {
+        afterDiv().secondPage();
+        $('.hard-memory-row').hide();
+        gamePlay(9, 15);
+        gameSetup().easyGame();
+    })
 })
--- js/populate.js
+++ js/populate.js
@@ -1,65 +1,48 @@
-function afterDiv () {
+function afterDiv() {
 
-	var scoreContents = $('.score-contents');
-	var homePage = $('.homepage');
-	var youWin = $('.you-win');
-	var main = $('main')
-	var after = $('.after-div');
+    var scoreContents = $('.score-contents');
+    var homePage = $('.homepage');
+    var youWin = $('.you-win');
+    var main = $('main')
+    var after = $('.after-div');
 
-	function firstToSecondPage () {
-		after.append($('#main').html())
-		window.setTimeout(function(){$('.top').prop('checked', true)}, 1);
-		adjustPiece();
-	}
+    function firstToSecondPage() {
+        after.append($('#main').html())
+        window.setTimeout(function() {
+            $('.top').prop('checked', true)
+        }, 1);
+        adjustPiece();
+    }
 
-	function adjustPiece () {
-		$('.piece').animate({
-			'padding': '.7%'
-		}, .001, function () {
-			$('.piece').css({
-				'padding': '7%'
-			})
-		})
-	}
+    function adjustPiece() {
+        $('.piece').animate({
+            'padding': '.7%'
+        }, .001, function() {
+            $('.piece').css({
+                'padding': '7%'
+            })
+        })
+    }
 
-	function secondToThirdPage () {
-		$('.you-win-input').prop('checked', true);
-	}
+    function secondToThirdPage() {
+        $('.you-win-input').prop('checked', true);
+    }
 
-	return {
-		firstPage: function () {
-			after.html($('#homepage').html());
-		},
+    return {
+        firstPage: function() {
+            after.html($('#homepage').html());
+        },
 
-		secondPage: function () {
-			firstToSecondPage();
-		},
+        secondPage: function() {
+            firstToSecondPage();
+        },
 
-		thirdPage: function() {
-			after.append($('#score-contents').html());
-			main.hide();
-			secondToThirdPage();
-		}
-	}
-
-	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        thirdPage: function() {
+            after.append($('#score-contents').html());
+            main.hide();
+            secondToThirdPage();
+        }
+    }
 
 
 
